a180b33e5d365fecaee9b41844049f261aad79c7
Jarek Zagórski <me.kubolski@gmail.com>
2022-12-24 14:53:26 +0100

    added new constructor

--- vectors.go
+++ vectors.go
@@ -11,73 +11,73 @@ import (
 type Vi2d = V2d[int]
 type Vf2d = V2d[float64]
 
-type Number interface{
-    constraints.Integer | constraints.Float
+type Number interface {
+	constraints.Integer | constraints.Float
 }
 
 type V2d[Type Number] struct {
-    X, Y Type
+	X, Y Type
 }
 
 func (this *V2d[Type]) Add(v V2d[Type]) {
-    this.X += v.X
-    this.Y += v.Y
+	this.X += v.X
+	this.Y += v.Y
 }
 
 func (this *V2d[Type]) Subtract(v V2d[Type]) {
-    this.X -= v.X
-    this.Y -= v.Y
+	this.X -= v.X
+	this.Y -= v.Y
 }
 
 func (this *V2d[Type]) Multiply(v V2d[Type]) {
-    this.X *= v.X
-    this.Y *= v.Y
+	this.X *= v.X
+	this.Y *= v.Y
 }
 
 func (this *V2d[Type]) Divide(v V2d[Type]) {
-    this.X /= v.X
-    this.Y /= v.Y
+	this.X /= v.X
+	this.Y /= v.Y
 }
 
-
+func NewVec[Type Number](x, y Type) *V2d[Type] {
+	return &V2d[Type]{X: x, Y: y}
+}
 
 // useful arithmetics
 func Add[Type Number](lhs, rhs V2d[Type]) (answer V2d[Type]) {
-    answer.X = lhs.X + rhs.X
-    answer.Y = lhs.Y + rhs.Y
+	answer.X = lhs.X + rhs.X
+	answer.Y = lhs.Y + rhs.Y
 	return
 }
 
 func Subtract[Type Number](lhs, rhs V2d[Type]) (answer V2d[Type]) {
-    answer.X = lhs.X - rhs.X
-    answer.Y = lhs.Y - rhs.Y
+	answer.X = lhs.X - rhs.X
+	answer.Y = lhs.Y - rhs.Y
 	return
 }
 
 func Multiply[Type Number](lhs, rhs V2d[Type]) (answer V2d[Type]) {
-    answer.X = lhs.X * rhs.X
-    answer.Y = lhs.Y * rhs.Y
+	answer.X = lhs.X * rhs.X
+	answer.Y = lhs.Y * rhs.Y
 	return
 }
 
 func Divide[Type Number](lhs, rhs V2d[Type]) (answer V2d[Type]) {
-    answer.X = lhs.X / rhs.X
-    answer.Y = lhs.Y / rhs.Y
+	answer.X = lhs.X / rhs.X
+	answer.Y = lhs.Y / rhs.Y
 	return
 }
 
-func Distance[Type Number](lhs, rhs V2d[Type]) (answer float64){
-    X := lhs.X - rhs.X
-    Y := lhs.Y - rhs.Y
+func Distance[Type Number](lhs, rhs V2d[Type]) (answer float64) {
+	X := lhs.X - rhs.X
+	Y := lhs.Y - rhs.Y
 
-    answer = math.Sqrt(float64(X * X + Y * Y))
-    return
+	answer = math.Sqrt(float64(X*X + Y*Y))
+	return
 }
-
-
 
 // general utility
 
 func (this *V2d[Type]) String() string {
-    return fmt.Sprintf("(%s, %s)", fmt.Sprint(this.X), fmt.Sprint(this.Y))
+	return fmt.Sprintf("(%s, %s)", fmt.Sprint(this.X), fmt.Sprint(this.Y))
 }
